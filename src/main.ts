aeaadf09b220fc23fef5c8fcbe7cd3235618b916
RalleeDev <109039505+RalleeDev@users.noreply.github.com>
2025-06-20 17:23:17 +0200

    Implemented the bare minimum for a discord bot to come online

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,10 @@
-console.log('Hello World');
+import { Client, Events, GatewayIntentBits }from 'discord.js';
+import 'dotenv/config'
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, readyClient => {
+    console.log('The discord bot is now online');
+});
+
+client.login(process.env.token)
